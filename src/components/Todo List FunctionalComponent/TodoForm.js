8f319a8a1cd9d6eebbeaa02732175928c75f2a66
adham <adham.assaf.sy@gmail.com>
2023-07-09 02:49:29 +0300

    first commit

---
+++ src/components/Todo List FunctionalComponent/TodoForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import shortid from 'shortid';
+
+function TodoForm(props){
+
+    const [text,setText] = useState("")
+   
+     const handleSubmit = (e)=>{
+        e.preventDefault();
+       
+        props.onSubmit({
+            id : shortid.generate(),
+            text : text,
+            complete : false
+        });
+       setText("")
+     }
+     return (
+        <form onSubmit={handleSubmit}>
+            <input className='input-field' type="text" onChange={(e)=>setText(e.target.value)} value={text}/>
+            <button className='btn' onClick={handleSubmit}>Add Todo</button>
+        </form>
+    );
+}
+
+
+
+
+export default TodoForm;
